7feb9ab7c9ee242a86c594ba3132197e8cdee320
berkland@us.ibm.com <berkland@us.ibm.com>
2016-07-05 15:15:09 -0500

    handle kernel_info msg

--- src/main/java/org/eclairjs/nashorn/server/Server.java
+++ src/main/java/org/eclairjs/nashorn/server/Server.java
@@ -11,6 +11,7 @@ package org.eclairjs.nashorn.server;
         import akka.http.javadsl.model.MediaTypes;
         import akka.http.javadsl.server.Marshaller;
         import akka.http.javadsl.server.RequestContext;
+        import com.fasterxml.jackson.annotation.JsonProperty;
         import com.fasterxml.jackson.core.JsonProcessingException;
         import com.fasterxml.jackson.databind.MapperFeature;
         import com.fasterxml.jackson.databind.ObjectMapper;
@@ -150,7 +151,14 @@ public class Server {
         try {
             Message_IN msg=objectMapper.readValue(text, Message_IN.class);
 
-            Message_IN out=respondMessage(msg.header,"status","iopub");
+            Message_IN out=null;
+            switch (msg.header.msg_type) {
+                case "kernel_info_request":
+                    out = kernelInfoRequest(msg);
+                    break;
+                    default:
+                        out=respondMessage(msg.header,new Content_Execution_State("busy"),"status","iopub");
+            }
 
             String json=objectMapper.writeValueAsString(out);
             return json;
@@ -161,7 +169,14 @@ public class Server {
 
     }
 
-    private static Message_IN respondMessage(MessageHeader parentMessage,String messageType,String channel)
+    private static Message_IN kernelInfoRequest(Message_IN msg) {
+
+        Content_Language_Info info=new Content_Language_Info(new Language_Info("scala","2.10.4"),"0.1.0.dev8-incubating-SNAPSHOT","IBM Spark Kernel","spark","5.0");
+        Message_IN out=respondMessage(msg.header,new Content_Execution_State("busy"),"kernel_info_reply","shell");
+        return out;
+    }
+
+    private static Message_IN respondMessage(MessageHeader parentMessage,Content content,String messageType,String channel)
     {
         String messageId=nextMessageID();
         Metadata metadata=new Metadata();
@@ -178,6 +193,7 @@ public class Server {
         returnMsg.msg_type=messageType;
         returnMsg.msg_id=messageId;
         returnMsg.channel=channel;
+        returnMsg.content=content;
 
         return returnMsg;
 
@@ -230,20 +246,80 @@ public class Server {
         public String channel;
         public Content content;
         public Metadata metadata;
-        public String [] buffers;
+        public String [] buffers=new String[0];
     }
 
     static class MessageHeader {
-        public String username;
+        public String username="";
         public String version;
-        public String session;
+        public String session="";
         public String msg_id;
         public String msg_type;
     }
 
     static class Content {
-        public String execution_state;
     }
+    static class Content_Execution_State extends Content {
+        public String execution_state;
+        Content_Execution_State(String state)
+        {this.execution_state=state;}
+    }
+    static class Language_Info {
+        public String name;
+        public String version;
+        Language_Info(String name,String version)
+        {
+            this.name=name;
+            this.version=version;
+        }
+    }
+    static class Content_Language_Info extends Content {
+        public Language_Info language_info;
+        public String implementation_version;
+        public  String banner;
+        public String implementation;
+        public String protocol_version;
+        Content_Language_Info(Language_Info language_info, String implementation_version, String banner, String implementation, String protocol_version) {
+            this.language_info=language_info;
+            this.implementation_version=implementation_version;
+            this.banner=banner;
+            this.implementation=implementation;
+            this.protocol_version=protocol_version;
+
+        }
+    }
+    static class Content_Execution_Count extends Content {
+        public int execution_count;
+        public String code;
+    }
+
+    static class Content_Status extends Content {
+        public String status;
+        public String [] payload=new String[0];
+        public Object  user_expressions = new Object();
+        public int execution_count;
+    }
+
+    static class ReturnData {
+        @JsonProperty("text/plain")
+        public String data;
+        ReturnData(String data)
+        {this.data=data;}
+    }
+
+    static class Content_Data extends Content {
+        public String status;
+        public String [] payload=new String[0];
+        public Object  metadata = new Object();
+        public int execution_count;
+    }
+
+
+//    {"language_info": {"name": "scala", "version": "2.10.4"}, "implementation_version": "0.1.0.dev8-incubating-SNAPSHOT", "banner": "IBM Spark Kernel", "implementation": "spark", "protocol_version": "5.0"}
+//    {"execution_count": 1, "code": "var SparkContext = require(EclairJS_Globals.NAMESPACE + '/SparkContext');\nvar jsc = new SparkContext(\"local[*]\", \"foo\");"}
+//    {"status": "ok", "payload": [], "user_expressions": {}, "execution_count": 1}
+//    {"data": {"text/plain": "EclairJS-nashorn 0.5 Spark 1.6.0"}, "metadata": {}, "execution_count": 2}
+
     static class Metadata {
         public String timestamp;
     }
